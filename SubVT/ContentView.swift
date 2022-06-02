27f8f168bf85037d8bc4c28d8c35eefa0ba4077b
kukabi <kutsalbilgin@gmail.com>
2022-06-02 17:31:23 +0300

    Data package integration and some initial bits.

--- SubVT/ContentView.swift
+++ SubVT/ContentView.swift
@@ -17,29 +17,7 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
+        Image("3DLogoLight")
     }
 
     private func addItem() {
--- SubVT/SubVTApp.swift
+++ SubVT/SubVTApp.swift
@@ -5,19 +5,29 @@
 //  Created by Kutsal Kaan Bilgin on 2.06.2022.
 //
 
+import Combine
+import SubVTData
 import SwiftUI
 
 @main
 struct SubVTApp: App {
     let persistenceController = PersistenceController.shared
+    var cancellables: Set<AnyCancellable> = []
     
-    init () {
-        for family in UIFont.familyNames.sorted() {
-            let names = UIFont.fontNames(forFamilyName: family)
-            print("Family: \(family) Font names: \(names)")
-        }
+    init() {
+        let service = ReportService()
+        let publisher = service.getEraReport(startEraIndex: 3391)
+        publisher
+            .sink { (response) in
+                if response.error != nil {
+                    print("Service call error: \(response.error!)")
+                } else {
+                    print("Service call success: \(response.value!)")
+                }
+            }
+            .store(in: &cancellables)
     }
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
